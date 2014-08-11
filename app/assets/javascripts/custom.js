da846f0448087e8b6deb16c4dfcd1945fce10cbf
Daniel Dresselhaus <ddresselhaus@gmail.com>
2014-08-11 17:56:54 -0400

    Fixed collapsing navbar for mobile

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,2 +1,17 @@
 
+$(document).ready(function() {
 
+	$("#newpost").on("click", function() {
+ 		$("#hidden-textarea").fadeToggle("fast", "linear");
+	});
+
+	$(".show-reply").on("click", function(event) {
+		var parentContainer = $(event.target).closest(".initial-post");
+
+		var	replyTextarea = parentContainer.find(".reply-form");
+		replyTextarea.fadeToggle("fast", "linear");
+	});
+
+});
+
+	
