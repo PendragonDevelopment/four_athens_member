20038b919d6dbf194303a7793ff9c2d2fb44e664
Daniel Dresselhaus <ddresselhaus@gmail.com>
2014-08-26 00:48:22 -0400

    more validation, this time JS validation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,10 +2,20 @@
 
 
 $(document).ready(function() {
+
+	//hide textareas
 	$("#hidden-textarea").hide();
 	$(".reply-form").hide();
-    
-	
+
+	$( "form" ).submit(function( event ) {
+		$("div.error").remove();
+
+  		if ( $( ":input:first" ).val() === '' ) {
+    		$(this).before('<div class="error alert alert-danger">Did you forget something?</div>');
+    		event.preventDefault();
+  		} 		
+	});
+
 
 	$(".alert-info").delay(4000).fadeOut("fast", "linear");
 
