69f193c2b1e835bfd9340e110647575c479f805d
Daniel Dresselhaus <ddresselhaus@gmail.com>
2014-08-26 01:54:50 -0400

    turns out my JS solution sucked. Reverting

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,15 +7,33 @@ $(document).ready(function() {
 	$("#hidden-textarea").hide();
 	$(".reply-form").hide();
 
+/*	
+		$("form").submit(function() {
+			console.log($(":input.form-control:first"));
+			var abort = false;
+			$(':input.form-control').each(function(){
+				if ($(this).val()==='') {
+					$(this).after('<div class="error alert alert-danger">Did you forget something?</div>');
+					abort = true
+				}
+			});
+			if (abort) {return false;} else {return true;}
+		});
+	
+
+
+
 	$( "form" ).submit(function( event ) {
 		$("div.error").remove();
-
-  		if ( $( ":input:first" ).val() === '' ) {
+		console.log($(this).closest(":input"));
+  		if ($(":input").val()==='') {
     		$(this).before('<div class="error alert alert-danger">Did you forget something?</div>');
     		event.preventDefault();
-  		} 		
+  		}
+  		else {return false;} 	
+  		return false;	
 	});
-
+*/
 
 	$(".alert-info").delay(4000).fadeOut("fast", "linear");
 
