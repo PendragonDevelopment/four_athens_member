da159b62a271ab62f13b18e434f6f6db15179007
Jordan Burke <me@jordankburke.com>
2015-03-16 15:29:56 -0400

    Added limit to Stripe call

--- app/validators/stripe_validator.rb
+++ app/validators/stripe_validator.rb
@@ -1,7 +1,7 @@
 class StripeValidator < ActiveModel::Validator
 
   def validate(user)
-    stripe_customers = Stripe::Customer.all
+    stripe_customers = Stripe::Customer.all(limit: 100)
     emails = stripe_customers[:data].collect { |customer| customer[:email] }
     unless emails.include?(user.email)
       user.errors[:email] << "- You are not yet a FourAthens member. Contact jim@fourathens.com to change this."
