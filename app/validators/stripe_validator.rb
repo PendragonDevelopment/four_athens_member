886b821b4f6a006c01f96b65e1bfa69e05bee45a
Jordan Burke <me@jordankburke.com>
2015-03-16 17:05:32 -0400

    Forgot one fucking thing

--- app/validators/stripe_validator.rb
+++ app/validators/stripe_validator.rb
@@ -6,7 +6,7 @@ class StripeValidator < ActiveModel::Validator
     stripe_customers = stripe_customers + Stripe::Customer.all(limit: 100, starting_after: last_customer.id).to_a.flatten
     last_customer = stripe_customers.last
     stripe_customers = stripe_customers + Stripe::Customer.all(limit: 100, starting_after: last_customer.id).to_a.flatten
-    emails = stripe_customers[:data].collect { |customer| customer[:email] }.compact!
+    emails = stripe_customers.collect { |customer| customer[:email] }.compact!
     unless emails.include?(user.email)
       user.errors[:email] << "- You are not yet a FourAthens member. Contact jim@fourathens.com to change this."
     end
