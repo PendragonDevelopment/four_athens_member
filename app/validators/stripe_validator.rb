bc028c7c05471e05cf78dd89c0df48b7f15d6463
Jordan Burke <me@jordankburke.com>
2015-03-16 15:57:24 -0400

    Updated validator

--- app/validators/stripe_validator.rb
+++ app/validators/stripe_validator.rb
@@ -1,8 +1,12 @@
 class StripeValidator < ActiveModel::Validator
 
   def validate(user)
-    stripe_customers = Stripe::Customer.all(limit: 100)
-    emails = stripe_customers[:data].collect { |customer| customer[:email] }
+    stripe_customers = Stripe::Customer.all(limit: 100).to_a
+    last_customer = stripe_customers.last
+    stripe_customers = stripe_customers + Stripe::Customer.all(limit: 100, starting_after: last_customer.id).to_a.flatten
+    last_customer = stripe_customers.last
+    stripe_customers = stripe_customers + Stripe::Customer.all(limit: 100, starting_after: last_customer.id).to_a.flatten
+    emails = stripe_customers[:data].collect { |customer| customer[:email] }.compact!
     unless emails.include?(user.email)
       user.errors[:email] << "- You are not yet a FourAthens member. Contact jim@fourathens.com to change this."
     end
